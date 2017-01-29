837a3b8e85004d9ba339f4301c26c987392cd572
BorreAudenaert <borreaudenaert@gmail.com>
2017-01-29 11:36:51 +0100

    Aanpassingen gedaan maar werken niet

--- src/me/nivyox/discord/Main.java
+++ src/me/nivyox/discord/Main.java
@@ -4,6 +4,7 @@ import me.nivyox.discord.Commands.HelpCommand;
 import me.nivyox.discord.Commands.MemeCommand;
 import me.nivyox.discord.Commands.PingCommand;
 import me.nivyox.discord.utils.CommandParser;
+import me.nivyox.discord.utils.DiscordChannelListener;
 import me.nivyox.discord.utils.StaticValues;
 
 
@@ -19,8 +20,11 @@ public class Main {
 
     public static JDA jda;
     public static final CommandParser parser = new CommandParser();
-    public static MemeCommand Meme;
     public static HashMap<String, Command> commands = new HashMap<String, Command>();
+
+
+
+
 
     public static void main(String[] args) {
         try {
@@ -37,23 +41,26 @@ public class Main {
         commands.put("ping", new PingCommand());
         commands.put("meme", new MemeCommand());
         commands.put("help", new HelpCommand());
+
+
+
+
     }
 
-    public static void handleCommand(CommandParser.CommandContainer cmd){
+    public static void handleCommand(CommandParser.CommandContainer cmd) {
         //System.out.println(cmd.invoke);
-        if(commands.containsKey(cmd.invoke)){
+        if (commands.containsKey(cmd.invoke)) {
             boolean safe = commands.get(cmd.invoke).called(cmd.args, cmd.event);
-            if (safe){
+            if (safe) {
                 commands.get(cmd.invoke).action(cmd.args, cmd.event);
                 commands.get(cmd.invoke).executed(safe, cmd.event);
-            }else{
+            } else {
                 commands.get(cmd.invoke).executed(safe, cmd.event);
             }
         }
 
     }
 
-    public HashMap<String, Command> getCommands(){
-        return commands;
-    }
+
+
 }
--- src/me/nivyox/discord/utils/DiscordChannelListener.java
+++ src/me/nivyox/discord/utils/DiscordChannelListener.java
@@ -1,35 +1,25 @@
-package me.nivyox.discord;
+package me.nivyox.discord.utils;
 
-import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.entities.MessageChannel;
-import net.dv8tion.jda.core.entities.User;
+import me.nivyox.discord.Main;
 import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 /**
- * Created by Niek on 1/17/2017.
+ * Created by Borre on 29/01/2017.
  */
 public class DiscordChannelListener extends ListenerAdapter {
 
-    @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         if (event.getMessage().getContent().startsWith("/")) {
             Main.handleCommand(Main.parser.parse(event.getMessage().getContent().toLowerCase(), event));
-
-
-            //User sender = event.getAuthor();
-            //Message message = event.getMessage();
-            //MessageChannel channel = event.getChannel();
-            //System.out.println(sender.getName() + " said > '" + message.getContent() + "' in " + channel.getName());
         }
     }
 
-
     @Override
     public void onReady(ReadyEvent event){
-
         //Main.log("status", "Logged in as: " + event.getJDA().getSelfUser().getName());
-
     }
+
+
 }
