90335733061608bd9e896337fb3846635d94a6d3
DI4MONDTOOL <gautier.de.bruijne@hotmail.com>
2017-02-07 15:11:30 +0100

    v1.1

--- OUDE BOT WERKT NIET/src/me/nivyox/discord/utils/StaticValues.java
+++
@@ -1,8 +0,0 @@
-package me.nivyox.discord.utils;
-
-/**
- * Created by Niek on 1/17/2017.
- */
-public class StaticValues {
-    public static String botkey = "MjcxMDEwOTMyMjU4MzczNjMy.C2z0gw.LwPb5C0beuD0DHCcw0ayLl0qxQ4";
-}
